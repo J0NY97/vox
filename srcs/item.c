daea115a8b3b645dfc3101b0c88775f84eb90b29
J0NY97 <jony.salmi97@gmail.com>
2022-04-18 13:17:48 +0300

    we are leaving it here

--- srcs/item.c
+++ srcs/item.c
@@ -41,16 +41,18 @@ int	water_placer(t_chunk_info *info, float *world_pos, int nth_from_source)
 
 	// Check that position is not occupied;
 	type = get_block_type_at_world_pos(info, world_pos);
-	if (!(type == GAS_AIR || (type > FLUID_WATER + nth_from_source && type < FLUID_WATER_7 + 1)))
+	if (!(type == GAS_AIR ||
+		(type > FLUID_WATER + nth_from_source && type < FLUID_WATER_7 + 1) ||
+		(type > FLORA_FIRST && type < FLORA_LAST)))
 		return (0);
 	
 	// Check that we have a block under this block;
 	type = get_block_type_at_world_pos(info, (float []) {world_pos[0], world_pos[1] - 1.0f, world_pos[2]});
 	if (type <= BLOCK_FIRST || type >= BLOCK_LAST)
 	{
-//		set_block_at_world_pos(info, world_pos, FLUID_WATER + nth_from_source);
-		water_placer(info, (float []){world_pos[0], world_pos[1] - 1.0f, world_pos[2]}, nth_from_source + 1);
-		return (1);
+		set_block_at_world_pos(info, world_pos, FLUID_WATER + nth_from_source);
+		water_placer(info, (float []){world_pos[0], world_pos[1] - 1.0f, world_pos[2]}, 0);
+		return (0);
 	}
 
 	// Set water block
@@ -61,17 +63,12 @@ int	water_placer(t_chunk_info *info, float *world_pos, int nth_from_source)
 
 	for (int j = 0; j < DIR_AMOUNT; j++)
 	{
-		if (j == DIR_UP)
+		if (j == DIR_UP || j == DIR_DOWN)
 			continue ;
 		for (int i = 1; i < 8 - nth_from_source; i++)
 		{
 			vec3_multiply_f(dir_mult, (float *)g_card_dir[j], i);
 			vec3_add(tmp, world_pos, dir_mult);
-			/*
-			if (j == DIR_DOWN) // reset nth_from_source
-				water_placer(info, tmp, 0);
-			else
-			*/
 			if (!water_placer(info, tmp, i + nth_from_source))
 				break ;
 		}
